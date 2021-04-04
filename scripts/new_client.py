caa40e95bbe16e60250cf2b3b74e5619957a7bfb
hyperpro <zhangbjb@126.com>
2021-04-03 23:02:01 -0500

    clean

--- scripts/new_client.py
+++ scripts/new_client.py
@@ -34,14 +34,15 @@ def sys_main():
         # In this example, we insert (y_id, field0) to the table
         random.seed(time.time())
         paras = [''.join(random.sample(string.ascii_letters + string.digits, 20)), '1']
-
+        print('Insert data:')
+        print(paras)
         # send request
         sender.sendOneRequest(host=replica_selection(req_id=req_id), type=QueryType.INSERT, db_data=paras, req_id=str(req_id))
 
     # print latency for each request. You can use this data to build latency profile for each replica server
     print('Here is latency:')
     latency_array = sender.getLatencies()
-    time.sleep(10)
+    # time.sleep(10)
     for i in latency_array:
         print('Latency for request' + str(i) + ':' + str(latency_array[i]))
 
